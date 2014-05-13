6ed0ce5a0c4e779b48dd693198b0498fd09d8ab8
dayAlone <andrey.slider@gmail.com>
2014-05-13 19:24:58 +0600

    Index

--- index.php
+++ index.php
@@ -81,72 +81,72 @@ catch(e) { }
           Мы верим в то, что сильный бренд <br>
           — это эмоциональный бренд.
         </p>
-        <p class="center">
+        <p class="center small-bottom-margin">
           <a href="/about/" class="big-button">
             и еще немного о нас
           </a>
         </p>
       </div>
     </div>
-    <div id="services" class="block bgcolor violet">
+    <div id="services" class="block bgcolor bright gray4">
       <div class="container">
-        <h3 class="title border-bottom">
-          Нам по силам  
+        <h3 class="title">
+          Наша компетенция  
         </h3>
         <?
           	$APPLICATION->IncludeComponent("bitrix:news.list", "services", array(
-	"IBLOCK_TYPE" => "content",
-	"IBLOCK_ID" => "2",
-	"NEWS_COUNT" => "6",
-	"SORT_BY1" => "SORT",
-	"SORT_ORDER1" => "ASC",
-	"SORT_BY2" => "ACTIVE_FROM",
-	"SORT_ORDER2" => "DESC",
-	"FILTER_NAME" => "",
-	"FIELD_CODE" => array(
-		0 => "CODE",
-		1 => "NAME",
-		2 => "PREVIEW_TEXT",
-		3 => "PREVIEW_PICTURE",
-		4 => "DETAIL_URL",
-		5 => "",
-	),
-	"PROPERTY_CODE" => array(
-		0 => "",
-		1 => "",
-	),
-	"CHECK_DATES" => "Y",
-	"DETAIL_URL" => "/services/#ELEMENT_CODE#/",
-	"AJAX_MODE" => "N",
-	"AJAX_OPTION_JUMP" => "N",
-	"AJAX_OPTION_STYLE" => "N",
-	"AJAX_OPTION_HISTORY" => "N",
-	"CACHE_TYPE" => "A",
-	"CACHE_TIME" => "3600",
-	"CACHE_FILTER" => "N",
-	"CACHE_GROUPS" => "N",
-	"PREVIEW_TRUNCATE_LEN" => "",
-	"ACTIVE_DATE_FORMAT" => "d-m-Y",
-	"SET_TITLE" => "N",
-	"SET_STATUS_404" => "N",
-	"INCLUDE_IBLOCK_INTO_CHAIN" => "N",
-	"ADD_SECTIONS_CHAIN" => "N",
-	"HIDE_LINK_WHEN_NO_DETAIL" => "N",
-	"PARENT_SECTION" => "",
-	"PARENT_SECTION_CODE" => "",
-	"INCLUDE_SUBSECTIONS" => "Y",
-	"PAGER_TEMPLATE" => "",
-	"DISPLAY_TOP_PAGER" => "N",
-	"DISPLAY_BOTTOM_PAGER" => "N",
-	"PAGER_TITLE" => "",
-	"PAGER_SHOW_ALWAYS" => "N",
-	"PAGER_DESC_NUMBERING" => "Y",
-	"PAGER_DESC_NUMBERING_CACHE_TIME" => "3600",
-	"PAGER_SHOW_ALL" => "N",
-	"AJAX_OPTION_ADDITIONAL" => ""
-	),
-	false
-);
+			"IBLOCK_TYPE" => "content",
+			"IBLOCK_ID" => "2",
+			"NEWS_COUNT" => "6",
+			"SORT_BY1" => "SORT",
+			"SORT_ORDER1" => "ASC",
+			"SORT_BY2" => "ACTIVE_FROM",
+			"SORT_ORDER2" => "DESC",
+			"FILTER_NAME" => "",
+			"FIELD_CODE" => array(
+				0 => "CODE",
+				1 => "NAME",
+				2 => "PREVIEW_TEXT",
+				3 => "PREVIEW_PICTURE",
+				4 => "DETAIL_URL",
+				5 => "",
+			),
+			"PROPERTY_CODE" => array(
+				0 => "",
+				1 => "",
+			),
+			"CHECK_DATES" => "Y",
+			"DETAIL_URL" => "/services/#ELEMENT_CODE#/",
+			"AJAX_MODE" => "N",
+			"AJAX_OPTION_JUMP" => "N",
+			"AJAX_OPTION_STYLE" => "N",
+			"AJAX_OPTION_HISTORY" => "N",
+			"CACHE_TYPE" => "A",
+			"CACHE_TIME" => "3600",
+			"CACHE_FILTER" => "N",
+			"CACHE_GROUPS" => "N",
+			"PREVIEW_TRUNCATE_LEN" => "",
+			"ACTIVE_DATE_FORMAT" => "d-m-Y",
+			"SET_TITLE" => "N",
+			"SET_STATUS_404" => "N",
+			"INCLUDE_IBLOCK_INTO_CHAIN" => "N",
+			"ADD_SECTIONS_CHAIN" => "N",
+			"HIDE_LINK_WHEN_NO_DETAIL" => "N",
+			"PARENT_SECTION" => "",
+			"PARENT_SECTION_CODE" => "",
+			"INCLUDE_SUBSECTIONS" => "Y",
+			"PAGER_TEMPLATE" => "",
+			"DISPLAY_TOP_PAGER" => "N",
+			"DISPLAY_BOTTOM_PAGER" => "N",
+			"PAGER_TITLE" => "",
+			"PAGER_SHOW_ALWAYS" => "N",
+			"PAGER_DESC_NUMBERING" => "Y",
+			"PAGER_DESC_NUMBERING_CACHE_TIME" => "3600",
+			"PAGER_SHOW_ALL" => "N",
+			"AJAX_OPTION_ADDITIONAL" => ""
+			),
+			false
+		);
 		?>
           
         
@@ -158,9 +158,9 @@ catch(e) { }
       </div>
     </div>
 
-    <div id="portfolio" class="block bgcolor">
+    <div id="portfolio" class="small block bgcolor gray6">
       <div class="container">
-        <h3 class="title border-bottom">
+        <h3 class="title no-bottom-margin">
           А еще мы покажем вам, что делали
         </h3>
         <p class="highlight">
@@ -225,7 +225,7 @@ catch(e) { }
 		?>
       </div>
       <div class="container">
-        <p class="center">
+        <p class="center no-bottom-margin">
           <a href="/portfolio/" class="big-button">
             этого явно мало
           </a>
@@ -233,9 +233,9 @@ catch(e) { }
       </div>
     </div>
 
-    <div id="partners" class="block">
+    <div id="partners" class="block bgcolor gray7 bright">
       <div class="container">
-        <h3 class="title border-bottom">
+        <h3 class="title">
           Спасибо, что вы с нами
         </h3>
         <div class="items">
