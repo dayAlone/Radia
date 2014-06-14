711a7024e475f0bb02db9074c57637593f6b8e45
dayAlone <andrey.slider@gmail.com>
2014-06-15 02:51:49 +0600

    Portfolio

--- bitrix/templates/radia/components/bitrix/news.list/portfolio_index/template.php
+++ bitrix/templates/radia/components/bitrix/news.list/portfolio_index/template.php
@@ -5,7 +5,7 @@ foreach($arResult["ITEMS"] as $arItem):
 	?><a href="<?=$arItem['DETAIL_PAGE_URL']?>" class="item" style="background-image: url(http://radia.ru<?=$arItem['PREVIEW_PICTURE']['SRC']?>)">
       <span class="over">
         <span class="text">
-          <span class="name"><?=str_replace('<br>','', $arItem['~NAME'])?></span><span class="line"></span>
+          <span class="name"><?=str_replace('<br>',' ', $arItem['~NAME'])?></span><span class="line"></span>
           <span class="type">
           	<?
               $res = CIBlockSection::GetByID($arItem["IBLOCK_SECTION_ID"]);
--- bitrix/templates/radia/components/bitrix/news.list/portfolio_list/template.php
+++ bitrix/templates/radia/components/bitrix/news.list/portfolio_list/template.php
@@ -17,7 +17,7 @@
                 <a href="<?=$arItem['DETAIL_PAGE_URL']?>" class="item" style="background-image: url(http://radia.ru<?=$arItem['PREVIEW_PICTURE']['SRC']?>)">
                   <span class="over">
                     <span class="text">
-                      <span class="name"><?=str_replace('<br>','', $arItem['~NAME'])?></span><span class="line"></span><span class="type">
+                      <span class="name"><?=str_replace('<br>',' ', $arItem['~NAME'])?></span><span class="line"></span><span class="type">
                         <?
                         $res = CIBlockSection::GetByID($arItem["IBLOCK_SECTION_ID"]);
                         if($ar_res = $res->GetNext())
